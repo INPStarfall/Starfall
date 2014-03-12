4d65e3f317c8402bf8e0ac645a3a017416fbe80e
Divran <arviddivran@gmail.com>
2014-03-12 17:48:21 +0100

    adds E2's gTableSafe
    so that you can make peer to peer games which are unhackable

--- lua/starfall/libs_sh/globaltables.lua
+++ lua/starfall/libs_sh/globaltables.lua
@@ -4,9 +4,9 @@
 local gtables_library, gtables_metamethods = SF.Libraries.Register("globaltables")
 
 SF.GlobalTables = {}
-
 SF.GlobalTables.Global = {}
 SF.GlobalTables.Players = {}
+SF.GlobalTables.Safe = {}
 
 --- Global table shared by all instances on the same side.
 -- @name gtables_library.global
@@ -25,10 +25,23 @@ hook.Add("PlayerDisconnected", "SF_GlobalTables_dc", function(ply)
 	SF.GlobalTables.Players[ply] = nil
 end)
 
+local util_CRC = util.CRC
+local tonumber = tonumber
+local function getHash(code)
+	return tonumber(util_CRC(code)) or -1
+end
+
 local oldindex = gtables_metamethods.__index
 function gtables_metamethods:__index(k)
 	if k == "player" then
 		return SF.GlobalTables.Players[SF.instance.player]
+	elseif k == "safe" then
+		local hash = getHash(SF.instance.source[SF.instance.mainfile])
+		if not SF.GlobalTables.Safe[hash] then 
+			SF.GlobalTables.Safe[hash] = {}
+		end
+		
+		return SF.GlobalTables.Safe[hash]
 	else
 		return oldindex[k]
 	end
