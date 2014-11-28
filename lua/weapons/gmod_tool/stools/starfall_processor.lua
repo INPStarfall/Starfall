4abfa4f9c4c5665da433820a6b68ce2748383218
Starkkz <matiasahermosilla@gmail.com>
2014-11-28 16:52:50 -0300

    Download functionality for the tool
    I tried to add the download functionality to the tool, I'm not sure if it works, I couldn't test it because auto-refresh wasn't working on my client. I was expecting you could make something better out of it, I copied part of the E2 code because I'm not really a gmod scripter.

--- lua/weapons/gmod_tool/stools/starfall_processor.lua
+++ lua/weapons/gmod_tool/stools/starfall_processor.lua
@@ -14,7 +14,7 @@ cleanup.Register( "starfall_processor" )
 
 if SERVER then
 	CreateConVar('sbox_maxstarfall_processor', 10, {FCVAR_REPLICATED,FCVAR_NOTIFY,FCVAR_ARCHIVE})
-	
+
 	function MakeSF( pl, Pos, Ang, model)
 		if not pl:CheckLimit( "starfall_processor" ) then return false end
 
@@ -31,6 +31,43 @@ if SERVER then
 		pl:AddCount( "starfall_processor", sf )
 
 		return sf
+	end
+
+	util.AddNetworkString("starfall_download")
+	function WireLib.StarfallDownload(ply, targetEnt, wantedfiles, uploadandexit)
+		if not IsValid(targetEnt) or targetEnt:GetClass() ~= "starfall_processor" then
+			WireLib.AddNotify(ply, "Invalid Starfall chip specified.", NOTIFY_ERROR, 7, NOTIFYSOUND_DRIP3)
+			return
+		end
+
+		if not IsValid(ply) or not ply:IsPlayer() then
+			error("Invalid player entity (wtf??). This should never happen. " .. tostring(ply), 0)
+		end
+
+		if not hook.Run("CanTool", ply, WireLib.dummytrace(targetEnt), "starfall_processor") then
+			WireLib.AddNotify(ply, "You're not allowed to download from this Expression (ent index: "..targetEnt:EntIndex()..").", NOTIFY_ERROR, 7, NOTIFYSOUND_DRIP3)
+			return
+		end
+
+		local main = targetEnt.instance.source
+		local datastr = von.serialize({ { targetEnt.name, main } })
+		local numpackets = math.ceil(datastr / 64000)
+
+		local n = 0
+		for i = 1, #datastr, 64000 do
+			timer.Simple(n, function ()
+				if not IsValid(targetEnt) then
+					return
+				end
+				net.Start("starfall_download")
+				net.WriteEntity(targetEnt)
+				net.WriteBit(uploadandexit or false)
+				net.WriteUInt(numpackets, 16)
+				net.WriteString(datastr:sub(i, i + 63999)
+				net.Send(ply)
+			end)
+			n = n + 1
+		end
 	end
 else
 	language.Add( "Tool.starfall_processor.name", "Starfall - Processor" )
@@ -58,7 +95,7 @@ function TOOL:LeftClick( trace )
 		end
 		return true
 	end
-	
+
 	self:SetStage(0)
 
 	local model = self:GetClientInfo( "Model" )
@@ -81,7 +118,7 @@ function TOOL:LeftClick( trace )
 	undo.Finish()
 
 	ply:AddCleanup( "starfall_processor", sf )
-	
+
 	if not SF.RequestCode(ply, function(mainfile, files)
 		if not mainfile then return end
 		if not IsValid(sf) then return end -- Probably removed during transfer
@@ -94,13 +131,26 @@ function TOOL:LeftClick( trace )
 end
 
 function TOOL:RightClick( trace )
-	if SERVER then self:GetOwner():SendLua("SF.Editor.open()") end
+	if SERVER then
+		if trace.Entity:isPlayer() then return false end
+
+		local player = self:GetOwner()
+		if IsValid(trace.Entity) and trace.Entity:GetClass() == "starfall_processor" then
+			self:Download(player, trace.Entity)
+		end
+		self:GetOwner():SendLua("SF.Editor.open()")
+	end
 	return false
 end
 
 function TOOL:Reload(trace)
 	return false
 end
+
+function TOOL:Download(ply, ent)
+	WireLib.StarfallDownload(ply, ent, nil, true)
+end
+
 
 function TOOL:DrawHUD()
 end
@@ -109,6 +159,33 @@ function TOOL:Think()
 end
 
 if CLIENT then
+
+	local buffer, count = "", 0
+	local current_ent
+	net.Receive("starfall_download", function (len)
+		local ent = net.ReadEntity()
+
+		if IsValid(current_ent) and IsValid(ent) and ent ~= current_ent then
+			buffer = ""
+			count = 0
+		end
+
+		local uploadandexit = net.ReadBit() ~= 0
+		local numpackets = net.ReadUInt(16)
+
+		buffer = buffer .. net.ReadString()
+		count = count + 1
+
+		if numpackets <= count then
+			local ok, ret = pcall(von.deserialize, buffer)
+			buffer, count = "", 0
+			if not ok then
+				WireLib.AddNotify(ply, "Starfall download failed! Error message:\n" .. ret, NOTIFY_ERROR, 7, NOTIFYSOUND_DRIP3)
+				return
+			end
+		end
+	end)
+
 	local function get_active_tool(ply, tool)
 		-- find toolgun
 		local activeWep = ply:GetActiveWeapon()
@@ -116,45 +193,45 @@ if CLIENT then
 
 		return activeWep:GetToolObject(tool)
 	end
-	
+
 	hook.Add("PlayerBindPress", "wire_adv", function(ply, bind, pressed)
 		if not pressed then return end
-	
+
 		if bind == "impulse 100" and ply:KeyDown( IN_SPEED ) then
 			local self = get_active_tool( ply, "starfall_processor" )
 			if not self then
 				self = get_active_tool( ply, "starfall_screen" )
 				if not self then return end
-				
+
 				RunConsoleCommand( "gmod_tool", "starfall_processor" ) -- switch back to processor
 				return true
 			end
-			
+
 			RunConsoleCommand( "gmod_tool", "starfall_screen" ) -- switch to screen
 			return true
 		end
 	end)
 
 	local lastclick = CurTime()
-	
+
 	local function GotoDocs(button)
 		gui.OpenURL("http://sf.inp.io") -- old one: http://colonelthirtytwo.net/sfdoc/
 	end
-	
+
 	function TOOL.BuildCPanel(panel)
 		panel:AddControl( "Header", { Text = "#Tool.starfall_processor.name", Description = "#Tool.starfall_processor.desc" } )
-		
+
 		local gateModels = list.Get( "Starfall_gate_Models" )
 		table.Merge( gateModels, list.Get( "Wire_gate_Models" ) )
-		
+
 		local modelPanel = WireDermaExts.ModelSelect( panel, "starfall_processor_Model", gateModels, 2 )
 		panel:AddControl("Label", {Text = ""})
-		
+
 		local docbutton = vgui.Create("DButton" , panel)
 		panel:AddPanel(docbutton)
 		docbutton:SetText("Starfall Documentation")
 		docbutton.DoClick = GotoDocs
-		
+
 		local filebrowser = vgui.Create("wire_expression2_browser")
 		panel:AddPanel(filebrowser)
 		filebrowser:Setup("starfall")
@@ -162,7 +239,7 @@ if CLIENT then
 		function filebrowser:OnFileOpen(filepath, newtab)
 			SF.Editor.editor:Open(filepath, nil, newtab)
 		end
-		
+
 		local openeditor = vgui.Create("DButton", panel)
 		panel:AddPanel(openeditor)
 		openeditor:SetText("Open Editor")
